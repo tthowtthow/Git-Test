758076e590dbd8e9246d7ec8be47bddcc98f01c6
tthowtthow <soobinkimpp@hanmail.net>
2021-12-30 18:34:55 +0900

    수빈 : 테스트

---
+++ src/Footballs2/Test.java
@@ -0,0 +1,11 @@
+package Footballs2;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("dddd");
+		System.out.println("dddd22");
+	}
+
+}
